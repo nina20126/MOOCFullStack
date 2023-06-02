aab98d914ee7faea18998cac374513b60b7b228a
nina20126 <nina20126@student.hamk.fi>
2023-06-02 21:39:12 +0300

    step3

--- Osa 1/kurssitiedot/src/App.js
+++ Osa 1/kurssitiedot/src/App.js
@@ -39,18 +39,23 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = 
+    { name: 'Fundamentals of React', exercises: 10 }
+  
+
+  const part2 = 
+    { name: 'Using props to pass data', exercises: 7 }
+  
+
+  const part3 = 
+    { name: 'State of a component', exercises: 14 }
+
 
   return (
     <div>
       <Header courseP = {course} />
-      <Content osa1 = {part1} harj1 = {exercises1} osa2 = {part2} harj2 = {exercises2} osa3 = {part3} harj3 = {exercises3}/>
-      <Total totalEx = {exercises1 + exercises2 + exercises3} />
+      <Content osa1 = {part1.name} harj1 = {part1.exercises} osa2 = {part2.name} harj2 = {part2.exercises} osa3 = {part3.name} harj3 = {part3.exercises} />
+      <Total totalEx = {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
