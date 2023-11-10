deec4fd10e668107ab7b504be44212b87942d43c
nina20126 <nina20126@student.hamk.fi>
2023-11-10 10:13:33 +0200

    puhelinluettelo step 3

--- Osa 2/puhelinluettelo/src/App.jsx
+++ Osa 2/puhelinluettelo/src/App.jsx
@@ -3,16 +3,21 @@ import './App.css'
 
 const App = () => {
   const [person, setPerson] = useState([
-    {name: 'Arto Hellas'}
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ])
-  const [newName, setNewName] = useState('')
 
-  // tapahtumankäsittelijäfunktio reagoi napin painamiseen
-  const addName = (event) => { // Parametrin event arvona on metodin kutsun aiheuttama
-    event.preventDefault() // estää lomakkeen oletusarvoisen toiminnan, sivun uudelleen latautumisen
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addName = (event) => { 
+    event.preventDefault() 
 
       const nameObject = {
         name: newName,
+        number: newNumber,
       }
 
       const allNames = person.map((all) => all.name)
@@ -20,15 +25,24 @@ const App = () => {
 
       if (findName) {
         alert(`${newName} is already added to phonebook `)
+        clearForm()
       } else {
         setPerson(person.concat(nameObject))
-        setNewName('')
+        clearForm()
       }
-
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const clearForm = () => {
+    setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -36,17 +50,21 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <div>
         {person.map(p => 
         <li style={{listStyleType: 'none'}} key={p.name} > 
-          {p.name}
+          {p.name} {p.number}
         </li>)}
       </div>
     </div>
