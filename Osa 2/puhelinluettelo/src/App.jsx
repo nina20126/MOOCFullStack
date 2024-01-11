d08c40819ac30552ad18b98d6d0511ebbd38b28b
nina20126 <nina20126@student.hamk.fi>
2024-01-11 15:53:14 +0200

    steps 7-9

--- Osa 2/puhelinluettelo/src/App.jsx
+++ Osa 2/puhelinluettelo/src/App.jsx
@@ -1,68 +1,96 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Persons from './components/Persons'
-import PersonForm from './components/PersonForm'
-import Search from './components/Search'
+import { useState, useEffect } from "react";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Search from "./components/Search";
+import peopleService from "./services/people";
 
 const App = () => {
-  const [person, setPerson] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchBarQuery, setSearchBarQuery] = useState('')
+  const [person, setPerson] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchBarQuery, setSearchBarQuery] = useState("");
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/person')
-      .then(response => {
-        setPerson(response.data)
-      })
-  }, [])
+    peopleService.getAll().then((initialPerson) => {
+      setPerson(initialPerson);
+      console.log(initialPerson);
+    });
+  }, []);
 
-  const addName = (event) => { 
-    event.preventDefault() 
+  const addName = (event) => {
+    event.preventDefault();
 
-      const nameObject = {
-        name: newName,
-        number: newNumber,
-      }
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+    };
 
-      const allNames = person.map((all) => all.name)
-      const findName = allNames.includes(newName)
+    const allNames = person.map((all) => all.name);
+    const findName = allNames.includes(newName);
 
-      if (findName) {
-        alert(`${newName} is already added to phonebook `)
-        clearForm()
-      } else {
-        setPerson(person.concat(nameObject))
-        clearForm()
-      }
-  }
+    if (findName) {
+      alert(`${newName} is already added to phonebook `);
+      clearForm();
+    } else {
+      peopleService.create(nameObject).then((returnedPerson) => {
+        setPerson(person.concat(returnedPerson));
+        clearForm();
+      });
+    }
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
+    setNewNumber(event.target.value);
+  };
 
   const clearForm = () => {
-    setNewName('')
-    setNewNumber('')
-  }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const deleteObject = (id) => {
+    const comfirmDelete = confirm(
+      `Are you sure you want to delete person ${id}?`
+    ); // This is causing the [Violation] 'click' handler -error/notification in the console.
+
+    if (comfirmDelete) {
+      peopleService
+        .deletePerson(id)
+        .then(() => {
+          console.log(`Deleted post with ID ${id}`);
+          setPerson(person.filter((p) => p.id !== id));
+        })
+        .catch((error) => {
+          console.error("Error deleting person:", error);
+        });
+    }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Search setSearchBarQuery={setSearchBarQuery} />
       <h2>Add new contact</h2>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
       <h2>Numbers</h2>
-      <Persons person={person} search={searchBarQuery} />
+      <Persons
+        person={person}
+        search={searchBarQuery}
+        deleteObject={deleteObject}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Osa 2/puhelinluettelo/src/components/Persons.jsx
+++ Osa 2/puhelinluettelo/src/components/Persons.jsx
@@ -1,13 +1,16 @@
-const Persons = ({ person, search }) => {
-    return(
-      <div>
-          {person.filter((p)=>
-            p.name.toLowerCase().includes(search)).map(p => 
-            <li style={{listStyleType: 'none'}} key={p.name} > 
-            {p.name} {p.number}
-          </li>)}
-        </div>
-    )
-  }
+const Persons = ({ person, search, deleteObject }) => {
+  return (
+    <div>
+      {person
+        .filter((p) => p.name.toLowerCase().includes(search))
+        .map((p) => (
+          <li style={{ listStyleType: "none" }} key={p.name}>
+            {p.name} {p.number}{" "}
+            <button onClick={() => deleteObject(p.id)}>Delete</button>
+          </li>
+        ))}
+    </div>
+  );
+};
 
-export default Persons
+export default Persons;
