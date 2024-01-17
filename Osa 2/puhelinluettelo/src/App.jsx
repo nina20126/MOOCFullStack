c6fc7f3a11077a0d50e0bac257ed7158a854db55
nina20126 <nina20126@student.hamk.fi>
2024-01-17 16:43:49 +0200

    step 11

--- Osa 2/puhelinluettelo/src/App.jsx
+++ Osa 2/puhelinluettelo/src/App.jsx
@@ -1,27 +1,30 @@
-import { useEffect, useState } from "react"
-import Person from "./components/Person"
-import NewPersonForm from "./components/NewPersonForm"
-import Search from "./components/Search"
-import personService from "./services/people"
+import { useEffect, useState } from "react";
+import Person from "./components/Person";
+import NewPersonForm from "./components/NewPersonForm";
+import Search from "./components/Search";
+import personService from "./services/people";
+import Notification from "./components/Notification";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [searchInput, setSearchInput] = useState("")
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    personService.getAll().then((initialPerson) => {
-      setPersons(initialPerson);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error)
-    })
-    
+    personService
+      .getAll()
+      .then((initialPerson) => {
+        setPersons(initialPerson);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   }, []);
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const personObject = {
       name: newName,
       number: newNumber,
@@ -30,86 +33,106 @@ const App = () => {
     const allNames = persons.map((all) => all.name);
     const findName = allNames.includes(newName);
 
-    const existingPerson = persons.find((person) => person.name === newName)
-    const existingPersonId = existingPerson.id
-    //console.log("Id:", existingPersonId)
+    const existingPerson = persons.find((person) => person.name === newName);
 
     if (findName) {
-      const updateNumber = window.confirm(`${newName} is already added to phonebook. Do you want to update number?`)
-      
-      if (updateNumber) {
-        const updatedPerson = {...existingPerson, number: newNumber}
-        updatePerson(existingPersonId, updatedPerson)
-      } else {
-        clearForm()
-      }
+      const existingPersonId = existingPerson.id;
+      const updateNumber = window.confirm(
+        `${newName} is already added to phonebook. Do you want to update number?`
+      );
 
-    } else {
-      personService.create(personObject)
-      .then((returnedPerson) => {
-        setPersons(persons.concat(returnedPerson));
+      if (updateNumber) {
+        const updatedPerson = { ...existingPerson, number: newNumber };
+        updatePerson(existingPersonId, updatedPerson);
+      } else {
         clearForm();
-      })
-      .catch((error) => {
-        console.error("Error creating person:", error);
-      })
-      
+      }
+    } else {
+      personService
+        .create(personObject)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+
+          setMessage(`Person '${newName}' added successfully.`);
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+          clearForm();
+        })
+
+        .catch((error) => {
+          console.error("Error creating person:", error);
+        });
     }
   };
 
   const clearForm = () => {
-    setNewName("")
-    setNewNumber("")
+    setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
+    setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
+    setNewNumber(event.target.value);
   };
 
   const handleSearch = (event) => {
-    event.preventDefault()
-    const searchValue = event.target.value
-    const searchValueLower = searchValue.toLowerCase()
+    event.preventDefault();
+    const searchValue = event.target.value;
+    const searchValueLower = searchValue.toLowerCase();
     setSearchInput(searchValueLower);
   };
 
   const deleteObject = (name, id) => {
     const confirmDelete = window.confirm(
       `Do you want to delete person ${name}?`
-    )
+    );
 
     if (confirmDelete) {
       personService
         .deletePerson(id)
         .then(() => {
-          const updatedPersons = persons.filter((person) => person.id !== id)
+          const updatedPersons = persons.filter((person) => person.id !== id);
           setPersons(updatedPersons);
           console.log("Deleted");
+
+          setMessage(`Person '${name}' deleted successfully.`);
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+          clearForm();
         })
+
         .catch((error) => {
-          console.error("Error deleting person:", error)
+          console.error("Error deleting person:", error);
         });
     }
   };
 
   const updatePerson = (id, updatedInfo) => {
     personService
-    .update(id, updatedInfo)
-    .then((returnedPerson) => {
-      setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
-      console.log("updated succesfully")
-    })
-    .catch(error => console.log("Update error: ", error))
-    clearForm()
-  }
+      .update(id, updatedInfo)
+      .then((returnedPerson) => {
+        setPersons(
+          persons.map((person) => (person.id !== id ? person : returnedPerson))
+        );
+        console.log("updated succesfully");
+        setMessage(`Person '${updatedInfo.name}' updated successfully.`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+      })
+      .catch((error) => console.log("Update error: ", error));
+    clearForm();
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <NewPersonForm
         addPerson={addPerson}
         newName={newName}
